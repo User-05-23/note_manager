325ba2ef2ddef3a8cf5966125a550e73de8adbab
User-05-23 <zibertyu23@mail.ru>
2025-01-21 19:44:06 +0300

    greetings.py

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = "Yurii"
+print(username)
+title = "Future It Specialist"
+print(title)
+content = ("Working in an IT company, performing various tasks. Career development")
+print(content)
+status = "At work"
+print(status)
+created_date = "21.01.2025"
+print(created_date)
+issue_date = "28.02.2025"
+print(issue_date)
