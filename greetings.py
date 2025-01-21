647a441e4c4701c376ef5e841acaf8ba9bea7695
User-05-23 <zibertyu23@mail.ru>
2025-01-21 19:52:19 +0300

    Add files via upload

--- greetings.py
+++ greetings.py
@@ -11,6 +11,17 @@ print(created_date)
 issue_date = "28.02.2025"
 print(issue_date)
 
+
 created_date2 = "21.01"
 issue_date2 = "28.02"
 print(created_date2, issue_date2)
+
+username = input("Введите имя: ")
+title = input("Заголовок заметки: ")
+content = input("Описание заметки: ")
+status = input("Статус заметки: ")
+created_date = input("Дата создания заметки: ")
+issue_date = input("Дата истечения заметки: ")
+
+created_date2 =input("21.01")
+issue_date2 = input("28.02")
